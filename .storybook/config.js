fdd557e7ddeddfe77bfa6e772d88b64b5dbafa8c
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-08-10 07:33:49 +0500

    :zap: added info addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,10 +2,16 @@ import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { withA11y } from '@storybook/addon-a11y';
+import { withInfo } from '@storybook/addon-info';
 
 /* Decorators configuration */
 addDecorator(withKnobs);
 addDecorator(withA11y);
+addDecorator(withInfo({
+  header: false,
+  text: 'React Pakistan - Icon Collection',
+  inline: true,
+}));
 addParameters({
   viewport: {
     viewports: {
