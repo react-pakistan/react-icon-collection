85974095fed0a698b13b21654c0f016fa1b90e0d
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-08-10 07:58:16 +0500

    :zap: setup custom theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,18 +3,25 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { withA11y } from '@storybook/addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import { addReadme } from 'storybook-readme';
+// import { addReadme } from 'storybook-readme';
+import customTheme from './customTheme';
 
 /* Decorators configuration */
 addDecorator(withKnobs);
 addDecorator(withA11y);
-addDecorator(addReadme);
+// addDecorator(addReadme);
 addDecorator(withInfo({
   header: false,
   text: 'React Pakistan - Icon Collection',
   inline: true,
 }));
 addParameters({
+  options: {
+    theme: customTheme,
+    hierarchySeparator: /\/|\./,
+    hierarchyRootSeparator: /\|/,
+    sidebarAnimations: true,
+  },
   viewport: {
     viewports: {
       ...INITIAL_VIEWPORTS,
