7c3927ef315e0ccd84b5a9f04074e4c2b5b36a68
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-08-10 19:16:45 +0500

    :zoo-grey icon set added

---
+++ scripts/.svgrrc-grey.js
@@ -0,0 +1,15 @@
+module.exports = {
+  icon: true,
+  svgoConfig: {
+    plugins: [
+      {
+        removeViewBox: false,
+      },
+      {
+        removeAttrs: {
+          attrs: ["fill"],
+        },
+      },
+    ],
+  },
+};
---
+++ scripts/generate-icons.sh
@@ -0,0 +1,2 @@
+svgr --template scripts/svgr-template.js --config-file scripts/.svgrrc-grey.js -d src/icons/zoo-grey --ext tsx assets/zoo-grey
+node scripts/generate-icons.js
