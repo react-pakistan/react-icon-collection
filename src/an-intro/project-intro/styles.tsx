a8c186ed44017449162eb00b8447b47959fdc885
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-09-08 09:59:14 +0500

    :sparkles: added into component

--- src/an-intro/project-intro/styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const ProjectIntroContainer = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-`;
