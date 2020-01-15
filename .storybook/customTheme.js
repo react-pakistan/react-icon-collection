746e90d0fd318dfb87d21fe62c28008e2f50d300
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-01-15 15:48:14 +0500

    :recycle: upgraded and fixed mode

--- .storybook/customTheme.js
+++ .storybook/customTheme.js
@@ -1,6 +1,7 @@
 import { create } from "@storybook/theming";
 
 export default create({
+  base: "light",
   brandTitle: "React Icon Collection",
   brandUrl: "https://github.com/Taimoormk/react-icon-collection",
   brandImage: "https://res.cloudinary.com/dq6hflqwx/image/upload/v1565548545/GitHub/React_Pakistan_Storybook_Logo.jpg",
