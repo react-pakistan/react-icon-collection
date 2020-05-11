2b8a3fe5fd58e39e80195770f371232c5690d5da
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-05-11 20:32:42 +0500

    :recycle: upgraded dependencies & eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: '@taimoormk/eslint-config-shared/ts-base/.eslintrc.json',
+  parser: 'babel-eslint',
+  env: {
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': ['error', {
+      allow: ['_id'],
+    }],
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
