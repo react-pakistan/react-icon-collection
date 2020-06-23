6ecb539a4fde5ebbf1d4a847c22755766e74b1f3
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-06-23 12:03:31 +0500

    :recycle: minor amends

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -6,9 +6,6 @@ import "@storybook/addon-viewport/register";
 import "@storybook/addon-backgrounds/register";
 import "@storybook/addon-a11y/register";
 import "@storybook/addon-links/register";
-import registerWithPanelTitle from "storybook-readme/registerWithPanelTitle";
 import "./google-analytics/register";
 
 window.STORYBOOK_GA_ID = "UA-145839845-1";
-
-registerWithPanelTitle("Docs");
